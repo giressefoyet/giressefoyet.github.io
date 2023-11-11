98a23c4077791e134c55374f1425a2a697a0ff06
Giresse FOYET <ffgire@gmail.com>
2023-11-11 09:40:19 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,1 +1,6 @@
-
+<?php
+    $retour = mail('ffgire@gmail.com', 'Envoi depuis la page Contact', $_POST['nom'],
+    , $_POST['email'] , $_POST['tel'], $_POST['sujet'], $_POST['message'], 'From: giressefoyet.github.io');
+    if ($retour)
+        echo '<p>Votre message a bien été envoyé.</p>';
+?>
