ed9c5494083d4ac7834663f29649923ac0b32700
Giresse FOYET <ffgire@gmail.com>
2023-11-12 23:46:24 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
-    if(isset($_POST["message"])){
+/*if(isset($_POST["message"])){
         $message="Ce message a été envoyer depuis la page contact du site giressefoyet.github.io
         Nom : ". $_POST["nom"] ."
         Email : ". $_POST["email"] ."
@@ -12,5 +12,35 @@
         if ($retour){
             echo '<p>Votre message a bien été envoyé.</p>';
         }
-    }
-    ?>  
+    }*/
+    ?>
+<?php   
+//si le bouton envoyer a été cliqué  
+if (isset($_POST["envoyer"])){   
+    //on recupère le nom  
+    $nom = $_POST["nom"];  
+    //on recupère le prenom  
+    $prenom = $_POST["prenom"];    
+    //on recupère l'adresse email  
+    $email = $_POST["email"];      
+    //on recupère l'objet du message  
+    $sujet = $_POST["sujet"];  
+    //on recupère le message  
+    $message = $_POST["message"];  
+      
+    $to = "ffgire@gmail.com";      
+    $headers = "From:" . $nom . " " . $prenom ." " . $email;    
+    //on envoie le message avec la fonction mail  
+    if (mail($to,$sujett,$message,$headers))   
+    //si le message a été envoyé, on le confirme  
+    {   
+    echo " ton message est bien envoyé.";    
+    }   
+    //sinon on n'affiche un message d'erreur  
+    else   
+    {   
+    echo "Une erreur s'est produite";   
+    }   
+    }        
+
+?>     
